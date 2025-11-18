4f47b6adf1772ca18e8e1f5d17a5312bfbf0178e
haneesh ali rizvi <145126674+syedahaneesh@users.noreply.github.com>
2025-11-18 11:31:51 +0500

    frontend code

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AppNavigator from "./AppNavigator";
+
+import CartProvider from "/context/CartContext";
+import WishlistProvider from "/context/WishlistContext";
+
+export default function App() {
+  return (
+    <CartProvider>
+      <WishlistProvider>
+        <AppNavigator />
+      </WishlistProvider>
+    </CartProvider>
+  );
+}
