304c2cbea23683fc44c707bc5c6cf346e6733ac0
haneesh ali rizvi <k224240@nu.edu.pk>
2026-05-11 16:15:05 +0500

    audio visual search fix

--- client/src/api/visualSearchService.js
+++ client/src/api/visualSearchService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
 // const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000';
-const API_BASE_URL = 'https://mightiest-unextolled-valeri.ngrok-free.dev' ;
+const API_BASE_URL = 'https://walrus-app-w43ss.ondigitalocean.app';
 
 export const performVisualSearch = async (imageUri) => {
   try {
